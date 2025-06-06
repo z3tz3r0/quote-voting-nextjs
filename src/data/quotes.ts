21f89ed2d0c8300f94d9d10cd36456678fecf1ac
z3tz3r0 <kittipan.wang@gmail.com>
2025-06-06 23:37:44 +0700

    feat: mock data for initial quote prevent empty screen

---
+++ src/data/quotes.ts
@@ -0,0 +1,46 @@
+import { QuoteData } from "@/types/quotes.type";
+
+export const quotesData: QuoteData[] = [
+  {
+    id: 1,
+    text: "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    author: "Anonymous",
+    category: "Funny",
+    likes: 42,
+  },
+  {
+    id: 2,
+    text: "I'm reading a book about anti-gravity. It's impossible to put down!",
+    author: "Dad Jokes Central",
+    category: "Funny",
+    likes: 38,
+  },
+  {
+    id: 3,
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    category: "Inspirational",
+    likes: 156,
+  },
+  {
+    id: 4,
+    text: "Life is what happens to you while you're busy making other plans.",
+    author: "John Lennon",
+    category: "Life",
+    likes: 89,
+  },
+  {
+    id: 5,
+    text: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates",
+    category: "Wisdom",
+    likes: 94,
+  },
+  {
+    id: 6,
+    text: "I haven't slept for ten days, because that would be too long.",
+    author: "Mitch Hedberg",
+    category: "Funny",
+    likes: 67,
+  },
+];
