c40db615aec0ca60628ffe1ca2ae8dafb29513c5
z3tz3r0 <kittipan.wang@gmail.com>
2025-06-06 23:34:14 +0700

    feat: add auth handler from NextAuth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "test@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (
+          credentials?.email === "test@example.com" &&
+          credentials?.password === "password"
+        ) {
+          return { id: "1", name: "Test User", email: "test@example.com" };
+        }
+        return null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signIn",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
