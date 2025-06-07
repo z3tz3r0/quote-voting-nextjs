0eb92dea56a449e55d8a48ac73bf4d6ebb899b8e
z3tz3r0 <kittipan.wang@gmail.com>
2025-06-08 03:24:57 +0700

    refactor(auth): Update SignInPage to handle async searchParams

--- src/app/auth/signIn/page.tsx
+++ src/app/auth/signIn/page.tsx
@@ -1,13 +1,18 @@
 import { LoginForm } from "@/components/LoginForm";
-import { SignInPageProps } from "@/types/loginForm.type";
 
-const SignInPage = ({ searchParams }: SignInPageProps) => {
+const SignInPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) => {
+  const { error, callbackUrl } = await searchParams;
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <LoginForm
         className="min-w-md mx-auto"
-        errorUrl={searchParams?.error}
-        callbackUrl={searchParams?.callbackUrl}
+        errorUrl={error}
+        callbackUrl={callbackUrl}
       />
     </div>
   );
